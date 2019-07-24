16651cb9b3ba11d95091f664ff05009a6160fb15
juna21-meet <juna21@meet.mit.edu>
2019-07-24 10:09:44 -0400

    Stopping here

---
+++ fruit_sorter.py
@@ -0,0 +1,15 @@
+user=input('what kind of fruit am I sorting?')
+if user =="Apples":
+    print("Apples in bin 1")
+elif user =="Oranges":
+          print("Oranges in bin 2")
+elif user =="Bananas":
+    print("Bananas in bin 3")
+          
+          
+          
+           
+           
+          
+
+          
